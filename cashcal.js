00e373697fda4e4697772e93b39e1588b075a4d3
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-17 14:24:48 +0530

    feat: dom selection

---
+++ cashcal.js
@@ -0,0 +1,7 @@
+var billAmt = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var calcBtn = document.querySelector(".calc-btn");
+var output = document.querySelector(".output-div");
+
+console.log (billAmt,cashGiven)
+
