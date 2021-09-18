f5353f66ef66af63a9545b5a06723f06c34e1a59
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-18 18:19:33 +0530

    feat: cash > bill added

--- cashcal.js
+++ cashcal.js
@@ -3,26 +3,44 @@ var cashGiven = document.querySelector("#cash-given");
 var calcBtn = document.querySelector(".calc-btn");
 var output = document.querySelector(".output-div");
 
-calcBtn.addEventListener("click",clickHandler);
+calcBtn.addEventListener("click", clickHandler);
 
-function clickHandler(){
-const input = inputVal();
+function clickHandler() {
+  const input = inputVal();
+  if (validateInput(input)) {
+    validateAmt(input);
+    calculate(input);
+  } else {
+    alert();
+  }
 
-
-console.log(input.billAmtVal,input.cashGivenVal);
+  console.log(input.billAmtVal, input.cashGivenVal);
 }
 
 function inputVal() {
-return{
-    billAmtVal : parseFloat(billAmt.value),
-    cashGivenVal : parseFloat(cashGiven.value),
-};   
+  return {
+    billAmtVal: parseFloat(billAmt.value),
+    cashGivenVal: parseFloat(cashGiven.value),
+  };
 }
 
-function validateInput(input){
- if(isNaN(input.billAmtVal) || isNaN(input.cashGivenVal))
- return false;
- if((input.billAmtVal<=0) ||   (input.cashGivenVal<=0))
- return false;
- return true;
+function validateInput(input) {
+  if (isNaN(input.billAmtVal) || isNaN(input.cashGivenVal)) return false;
+  if (input.billAmtVal <= 0 || input.cashGivenVal <= 0) return false;
+  return true;
+}
+
+function validateAmt(input) {
+  if (input.cashGivenVal > input.billAmtVal) return true;
+  else {
+    output.innerText = "Cashgiven should be greater than Billamount.";
+  }
+}
+
+function calculate() {
+  console.log("yayy");
+}
+
+function alert() {
+  output.innerText = "Enter positive values.";
 }
